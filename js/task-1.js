d971217fffdf8d84384ebf4193c302cfad8a6ee1
zaharius <zaharius.ltd@gmail.com>
2020-07-20 00:01:57 +0300

    create task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,11 @@
+'use strict';
+
 const logItems = function(array) {
-  // твой код
+  for (let i = 0; i < array.length; i += 1) {
+    console.log(`${i+1} ${array[i]}`);
+  }
 };
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
 logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
 
 logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
--- js/task-7.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const arrs = ['mango', 'poly', 'teddy'];
-
-for (const arr of arrs) {
-    console.log(arr)
-}
