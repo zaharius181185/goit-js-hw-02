64789a819481f6a6c25f30874b6fb8317f3e1f23
zaharius <zaharius.ltd@gmail.com>
2020-07-20 00:57:57 +0300

    create task-5

---
+++ js/task-5.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const checkForSpam = function(message) {
+    let messageSplit = message.toLowerCase();
+    return messageSplit.includes("spam") || messageSplit.includes("sale");
+};
+
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
+
+
+
+
+
+
+
+
+// Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку. Функция проверяет ее на содержание слов spam и sale. Если нашли зарещенное слово то функция возвращает true, если запрещенных слов нет функция возвращает false. Слова в строке могут быть в произвольном регистре.
