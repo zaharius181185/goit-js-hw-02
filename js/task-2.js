096b276b7fecae076f2d42e705d6b0da5a98a3eb
zaharius <zaharius.ltd@gmail.com>
2020-07-20 00:18:52 +0300

    create task-2

---
+++ js/task-2.js
@@ -0,0 +1,36 @@
+'use strict'
+
+
+const calculateEngravingPrice = function(message, pricePerWord) {
+    return message.split(' ').length * pricePerWord
+};
+  
+console.log(
+calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    10,
+),
+); // 80
+
+console.log(
+calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    20,
+),
+); // 160
+
+console.log(
+calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+); // 240
+
+console.log(
+calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+); // 120
+
+
+
+
+
+
+
+// Напиши скрипт подсчета стоимости гравировки украшений. Для этого создай функцию calculateEngravingPrice(message, pricePerWord) принимающую строку (в строке будут только слова и пробелы) и цену гравировки одного слова, и возвращающую цену гравировки всех слов в строке.
