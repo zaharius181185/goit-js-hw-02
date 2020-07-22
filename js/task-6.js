18baba007530a9daf01937a1125d93c371754592
zaharius <zaharius.ltd@gmail.com>
2020-07-23 01:03:32 +0300

    edit task-6

--- js/task-6.js
+++ js/task-6.js
@@ -5,14 +5,15 @@ let total = 0;
 
 
 do {
-    input = prompt('Введите чисто.')
-    if(input !== null) {
-        if(Number.isNaN(+input)) {
-            alert('Было введено не число, попробуйте еще раз.');
-            continue;
-        }
-        numbers.push(+input)
+    input = prompt('Введите чисто.');
+
+    if(isNaN(input)) {
+        alert('Было введено не число, попробуйте еще раз.');
+        continue;
     }
+
+    numbers.push(+input);
+
 } while (input !==null)
 
 if(numbers.length) {
