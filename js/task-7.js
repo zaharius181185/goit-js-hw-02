8aeb2fa7888b3a3913e252959bb4b66f40f419ad
zaharius <zaharius.ltd@gmail.com>
2020-07-23 01:10:03 +0300

    changes task-7

--- js/task-7.js
+++ js/task-7.js
@@ -2,20 +2,27 @@
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 
 const isLoginValid = function(login) {
-  // твой код
+  return login.length >= 4 && login.length <= 16;
 };
 
 const isLoginUnique = function(allLogins, login) {
-  // твой код
+  return allLogins.includes(login);
 };
 
 const addLogin = function(allLogins, login) {
-  // твой код
+  if (!isLoginValid(login)) {
+    return "Ошибка! Логин должен быть от 4 до 16 символов";
+  }
+
+  if (isLoginUnique(allLogins, login)) {
+    return "Такой логин уже используется!";
+  }
+
+  logins.push(login);
+  return "Логин успешно добавлен!";
+  
 };
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
 console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
 console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
 console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
